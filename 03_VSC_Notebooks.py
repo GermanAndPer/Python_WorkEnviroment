9ecda3c10fb522d0a7de201b9d8923e7e8a5c083
GermanAndPer <germanandresperez1@gmail.com>
2025-06-08 16:14:50 -0600

    Add file NoteBooks for VSC

--- 03_VSC_Notebooks.py
+++
@@ -1,6 +0,0 @@
-'''
-Despues de haber instalado todas las aplicaciones, de manera automatica si quisieramos ejecutar un codigo de Python lo hariamos.
-En caso de que no deje, solo seria necesario que elijamos un interprete.
-VSC nos estará guiando para lograrlo.
-'''
-print("Hello, World")
