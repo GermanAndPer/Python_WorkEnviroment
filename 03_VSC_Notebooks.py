3e87454c32b6c34e60e5fee02cfda0ec4c26d47d
GermanAndPer <germanandresperez1@gmail.com>
2025-06-08 15:18:56 -0600

    Add Python file

---
+++ 03_VSC_Notebooks.py
@@ -0,0 +1,6 @@
+'''
+Despues de haber instalado todas las aplicaciones, de manera automatica si quisieramos ejecutar un codigo de Python lo hariamos.
+En caso de que no deje, solo seria necesario que elijamos un interprete.
+VSC nos estará guiando para lograrlo.
+'''
+print("Hello, World")
